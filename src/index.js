9d331c06075124981af7348df4d2641f93defc8a
ahmetulutas <englishlectures.stocks@gmail.com>
2022-01-19 00:23:37 +0300

    trying persist-store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import store from "./store";
 import App from "./App";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
+let persistor = persistStore(store);
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <StrictMode>
-    <Provider store={store}>
-    <App>
-
-    </App>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+                  <App></App>
+        </PersistGate>
+      </Provider>
   </StrictMode>,
   rootElement
 )
