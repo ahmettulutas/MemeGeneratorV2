410110d093b300d63abc8023ad120546e213ced6
ahmetulutas <englishlectures.stocks@gmail.com>
2022-01-19 11:06:21 +0300

    removing persist store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import { Provider } from "react-redux";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import store from "./store";
-import persistor from "./store";
+//import {persistor} from "./store";
 import App from "./App";
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+//import { persistStore } from 'redux-persist';
+//import { PersistGate } from 'redux-persist/integration/react';
 
-let persistor = persistStore(store);
 const rootElement = document.getElementById("root");
 
+ReactDOM.render(
+  <StrictMode>
+      <Provider store={store}>
+          <App></App>
+      </Provider>
+  </StrictMode>,
+  rootElement
+)
+/* 
 ReactDOM.render(
   <StrictMode>
       <Provider store={store}>
@@ -19,4 +27,4 @@ ReactDOM.render(
       </Provider>
   </StrictMode>,
   rootElement
-)
+) */
--- src/store.js
+++ src/store.js
@@ -1,9 +1,30 @@
   import { configureStore } from '@reduxjs/toolkit'
   import loadMemesSlice from "./features/loadMemes/loadMemesSlice";
   import filteredMemeSlice from "./features/editMeme/memeFormSlice";
+  import logger from 'redux-logger'
+  import thunk from 'redux-thunk'
 
     const store = configureStore({reducer:{
       loadMemesSlice: loadMemesSlice,
       filteredMemeSlice: filteredMemeSlice,
     }})
   export default store;
+/*
+const persistConfig = { // configuration object for redux-persist
+    key: 'root',
+    storage, // define which storage to use
+}  
+const rootReducer = combineReducers({
+  loadMemesSlice: loadMemesSlice,
+  filteredMemeSlice: filteredMemeSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [thunk, logger]
+  })
+  export const  persistor = persistStore(store);
+  export default store;
+*/
