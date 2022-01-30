8d90fb482c6db351b851bad5f233ce1447209b7e
ahmetulutas <englishlectures.stocks@gmail.com>
2022-01-30 23:29:03 +0300

    done with this project

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer className='footer-main'>
             <a target="_blank" rel="noreferrer" href="https://github.com/ahmettulutas"> check my github</a>
+            <p>AhmetUlutaş</p>
         </footer>
     );
 }
--- src/features/loadMemes/displayMemes.js
+++ src/features/loadMemes/displayMemes.js
@@ -29,7 +29,7 @@ const DisplayMemes = () => {
       return (
           <main className="all-memes-main-content">
               <h1>Loading...</h1>
-              <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_szlepvdh.json"  background="transparent"  speed="1"  style={{width: "300px", height: "300px;"}}  loop autoplay></lottie-player>;
+              <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_szlepvdh.json"  background="transparent"  speed="1"  style={{width: "80%", height: "300px;"}}  loop autoplay></lottie-player>;
           </main>
       )}
     if (fulfilled && allMemes) {
